a7dce50a6bbde55454abad4577ba969a907f4d5b
hasteds <tin.petr@gmail.com>
2024-03-09 10:32:21 +0100

    09.03-2

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const app = Vue.createApp({
     //template: '<h2>I am the template</h2>'
     data() {
         return {
+            url: 'http://www.net.hr',
             title: 'Star trek TNG',
             author: 'Gene Rodenberry',
             age: 67,
-            showBooks: true
+            showBooks: true,
+            x: 0,
+            y: 0,
+            books: [
+                {title: 'Star Wars', author: 'George Lucas', age: 82, img: 'images/1.jpg'},
+                {title: 'Star Trek', author: 'Gene Rodenberry', age: 88, img: 'images/2.jpg'},
+                {title: 'Dune', author: 'Frank Herbert', age: 65, img: 'images/3.jpg'}
+            ]
         }
     },
     methods: {
@@ -15,8 +23,15 @@ const app = Vue.createApp({
         toggleShowBook() {
             this.showBooks = !this.showBooks
         },
-        handleEvent() {
-            console.log('event')
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMouseMove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
